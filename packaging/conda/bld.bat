f3b4d5d7b6097c6426d10c56f06d4abeac9e3dd6
bruno <bruno.guerraz@orange.com>
2023-06-30 10:56:11 +0200

    Remove jobs option in CMakePreset
    Add --paralel flag for each cmake build

--- packaging/conda/bld.bat
+++ packaging/conda/bld.bat
@@ -1,3 +1,3 @@
 % REM We use ninja for generator because VS does not work with conda build
 cmake --preset windows-msvc-release -G "Ninja" -DBUILD_JARS=OFF -DTESTING=OFF
-cmake --build --preset windows-msvc-release --target MODL --target MODL_Coclustering
+cmake --build --preset windows-msvc-release --parallel --target MODL --target MODL_Coclustering
--- packaging/conda/build.sh
+++ packaging/conda/build.sh
@@ -1,3 +1,3 @@
 # !/bin/bash
 cmake --preset linux-gcc-release -DBUILD_JARS=OFF -DTESTING=OFF
-cmake --build --preset linux-gcc-release --target MODL --target MODL_Coclustering
+cmake --build --preset linux-gcc-release --parallel --target MODL --target MODL_Coclustering
