aa557de9dd2284512ca606d960fa12639523c77b
bruno <bruno.guerraz@orange.com>
2023-06-30 14:02:57 +0200

    build conda package for macOS
    add platform dependant code in build.sh and install-khiops.sh for 2 objectives:
     - to handle with the different presets name
     - to specify the out-of-conda clang compiler in the conda build. There is an issue to use the conda's one.

--- packaging/conda/install-khiops.sh
+++ packaging/conda/install-khiops.sh
@@ -3,7 +3,16 @@
 # Echo all output
 set -x
 
+
+# The binary location depends on the preset name used at the configure step (Cf. build.sh)
+if [ "$(uname)" == "Darwin" ]
+then
+    BUILD_DIR="macos-clang-release"
+else
+    BUILD_DIR="linux-gcc-release"
+fi
+
 # Copy the MODL binaries to the anaconda PREFIX path
 mkdir -p $PREFIX/bin
-cp build/linux-gcc-release/bin/MODL $PREFIX/bin
-cp build/linux-gcc-release/bin/MODL_Coclustering $PREFIX/bin
+cp build/$BUILD_DIR/bin/MODL $PREFIX/bin
+cp build/$BUILD_DIR/bin/MODL_Coclustering $PREFIX/bin
