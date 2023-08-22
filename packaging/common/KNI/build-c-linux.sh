f877b35f02d4ed8ddd034044450f69b0c77ee7da
bruno <bruno.guerraz@orange.com>
2023-08-22 10:04:56 +0200

    Add script to kni-tutorial and test-kni
    add script to get the same script in the tutorial and in the action test-kni

---
+++ packaging/common/KNI/build-c-linux.sh
@@ -0,0 +1,2 @@
+gcc -o KNIRecodeFile tutorial/cpp/KNIRecodeFile.c -I /usr/include/ -lKhiopsNativeInterface  -ldl
+gcc -o KNIRecodeMTFiles tutorial/cpp/KNIRecodeMTFiles.c  -I /usr/include/ -lKhiopsNativeInterface -ldl
