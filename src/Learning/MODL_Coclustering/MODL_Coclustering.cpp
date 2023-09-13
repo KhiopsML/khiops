3c04ff3ddcd636ddcb6a2add24198dc7cf1f507a
Marc Boullé <marc.boulle@orange.com>
2023-09-13 17:25:25 +0200

    Collect profiling stats for coclustering IV optimization
    Nouvelle classe Profiler ajoutee dans la librairie KWData
    - fichier de stats via la collecte des stats par methode tracee (nombre d'appels, duree moyenne)
    - ajout optionnel de traces dans un fichier json, pour avoir la dynamique des appels
    - declaration d'un pointeur vers le fichier de trace JSONFile plutot qu'un objet directement
      - cet objet est alloue/desalloue le temps d'un session de profiling
      - cela permet de bien controler la memoire, meme si l'on declare des Profiler globaux en statique
      - Attention: il ne faut pas utiliser les JSONFile en statique directement ou indirectement
        - la classe JSONFile utiliser deja une instance en statique pour gerer l'inititialisation de
          des structure globales, et cela ne pose pas de probleme a l'allocateur memoire
        - par contre, une deuxime instance de JSONFile en statique creer des problemes pour l'allocateur,
          qui diagnostique a tor des fuites memoire (les  variable statique snt allouee avant le debut
          et desalloue apres le fin du main()): resoudre ce probleme est long et sans interet
    
    Utilisation du Profiler via des methodes de classe de KWDataGridOptimizer
    - uniquement methode GetProfiler
      - Acces au profiler global permettant d'enregistrer toute une session de profiling
        de l'optimisation d'une grille de coclustering
    - choix des methodes a profiler et des noms externes pour faciliter leur interpretation
      - Compute coclustering
        - (CCCoclusteringBuilder::ComputeCoclustering)
      - Optimize VarPart prepartition
        - (CCCoclusteringBuilder::OptimizeVarPartDataGrid)
      - Optimize last granularity
        - (KWDataGridOptimizer::OptimizeDataGrid)
      - VNS optimize
        - (KWDataGridVNSOptimizer::IterativeVNSOptimizeDataGrid)
      - Optimize solution
        -(KWDataGridVNSOptimizer::VNSOptimize(VarPart)DataGrid)
      - Pre-optimization
        - (KWDataGridVNSOptimizer::OptimizeSolution)
      - Greedy merge optimization
        - (KWDataGridVNSOptimizer::OptimizeSolution)
      - Post-optimization
        - (KWDataGridVNSOptimizer::OptimizeSolution)
      - Post-optimization IV
        - (KWDataGridVNSOptimizer::VNSOptimizeVarPartDataGrid)
      - Optimize granularity
        - (KWDataGridOptimizer::OptimizeDataGrid)
    - pilotage global depuis CCCoclusteringBuilder::ComputeCoclustering
      - lancement ou non du profiling
      - stats sur les donnees de la grille initiale
    
    Tests sur LearningTest\TestCoclustering\y_CoclusteringIV_Standard et LearningTest\TestCoclustering\Standard\Iris

--- src/Learning/MODL_Coclustering/MODL_Coclustering.cpp
+++ src/Learning/MODL_Coclustering/MODL_Coclustering.cpp
@@ -34,12 +34,12 @@ int main(int argc, char** argv)
 	Global::ActivateSignalErrorManagement();
 
 	// Choix du repertoire de lancement pour le debugage sous Windows (a commenter apres fin du debug)
-	SetWindowsDebugDir("y_CoclusteringIV_Standard", "IrisLight");
+	//SetWindowsDebugDir("y_CoclusteringIV_Standard", "IrisLight");
 	//DDD SetWindowsDebugDir("y_CoclusteringIV_Standard", "SimplifyCoclustering");
-	//SetWindowsDebugDir("Standard", "Iris");
+	SetWindowsDebugDir("Standard", "Iris");
 
 	// Point d'arret sur l'allocation d'un bloc memoire
-	// MemSetAllocIndexExit(17362);
+	// MemSetAllocIndexExit(77);
 
 	// Lancement du projet
 	learningProject.Start(argc, argv);
