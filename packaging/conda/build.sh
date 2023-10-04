6863b89f58d7dc54be591e0e6bcd821fe785bcf7
Felipe Olmos <luisfelipe.olmosmarchant@orange.com>
2023-10-04 11:23:34 +0200

    Make presets defaults coherent with CMakeLists.txt

--- packaging/conda/build.sh
+++ packaging/conda/build.sh
@@ -4,9 +4,9 @@
 # ld: unsupported tapi file type '!tapi-tbd' in YAML file '/Applications/Xcode_14.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/libSystem.tbd' for architecture x86_64
 if [ "$(uname)" == "Darwin" ]
 then
-    cmake --preset macos-clang-release -DBUILD_JARS=OFF -DTESTING=OFF -DCMAKE_CXX_COMPILER=/usr/bin/clang++
+    cmake --preset macos-clang-release -DCMAKE_CXX_COMPILER=/usr/bin/clang++
     cmake --build --preset macos-clang-release --parallel --target MODL MODL_Coclustering
 else
-    cmake --preset linux-gcc-release -DBUILD_JARS=OFF -DTESTING=OFF
+    cmake --preset linux-gcc-release
     cmake --build --preset linux-gcc-release --parallel --target MODL MODL_Coclustering
 fi
