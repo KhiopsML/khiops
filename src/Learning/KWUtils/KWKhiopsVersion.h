703c0273cae55abb89c7a65d4dfb1b2a3c46cec2
Felipe Olmos <92923444+folmos-at-orange@users.noreply.github.com>
2023-11-29 17:39:18 +0100

    Bump source version to 10.2.0

--- src/Learning/KWUtils/KWKhiopsVersion.h
+++ src/Learning/KWUtils/KWKhiopsVersion.h
@@ -10,7 +10,7 @@
 // dans le TaskManager de Windows (par exemple)
 
 // Version de Khiops
-#define KHIOPS_VERSION str(10.1.5)
+#define KHIOPS_VERSION str(10.2.0)
 
 // Copyright
 #define KHIOPS_COPYRIGHT_LABEL str((c)2023 Orange - All rights reserved.)
