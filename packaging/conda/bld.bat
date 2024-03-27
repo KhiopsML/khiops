cc45b254c5d20cef716e6207911f7fa449d90b63
Felipe Olmos <92923444+folmos-at-orange@users.noreply.github.com>
2024-03-27 16:08:52 +0100

    WIP Update conda packaging

--- packaging/conda/bld.bat
+++ packaging/conda/bld.bat
@@ -1,2 +1,13 @@
-cmake --preset windows-msvc-release
+REM Echo all output
+@echo on
+
+REM Build the Khiops binaries
+cmake --preset windows-msvc-release -DBUILD_JARS=OFF -DTESTING=OFF
 cmake --build --preset windows-msvc-release --parallel --target MODL MODL_Coclustering
+
+REM Copy the MODL binaries to the Conda PREFIX path
+mkdir %PREFIX%\bin
+copy build\windows-msvc-release\bin\MODL.exe %PREFIX%\bin
+copy build\windows-msvc-release\bin\MODL_Coclustering.exe %PREFIX%\bin
+
+if errorlevel 1 exit 1
