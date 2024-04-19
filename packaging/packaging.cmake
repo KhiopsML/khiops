5c713785e4618a2ee41dd0d1253af0337a88049c
bruno <97033386+bruno-at-orange@users.noreply.github.com>
2024-04-19 10:20:18 +0200

    remove post-installations script from khiops packages (#245)
    * remove post-installations script from khiops packages
    
    Java configuration is in khiops-env in the package khiops-core. It is not updated by khiops package anymore.
    The trick is that this configuration is triggered by the presence of khiops.jar
    
    On the docker files, We pin the jdk version to the smallest one on each distro (it matches most of the time the default-jdk package)
    The idea is to build norm.jar with the oldest jdk to have a jar compatible with all jre. Ideally we should build with jdk 1.8 on all
    distros but this package is not available everywhere.

--- packaging/packaging.cmake
+++ packaging/packaging.cmake
@@ -107,7 +107,6 @@ set(CPACK_DEBIAN_KHIOPS_CORE_PACKAGE_RECOMMENDS "khiops, khiops-visualization")
 set(CPACK_DEBIAN_KHIOPS_KNI_RECOMMENDS kni-doc)
 
 # packages posinst and triggers
-set(CPACK_DEBIAN_KHIOPS_PACKAGE_CONTROL_EXTRA "${PROJECT_SOURCE_DIR}/packaging/linux/debian/khiops/postinst")
 set(CPACK_DEBIAN_KHIOPS_CORE_PACKAGE_CONTROL_EXTRA "${PROJECT_SOURCE_DIR}/packaging/linux/debian/khiops-core/postinst")
 set(CPACK_DEBIAN_KNI_PACKAGE_CONTROL_EXTRA "${PROJECT_SOURCE_DIR}/packaging/linux/debian/kni/triggers")
 
@@ -148,7 +147,6 @@ set(CPACK_RPM_KHIOPS_CORE_PACKAGE_REQUIRES "util-linux")
 
 # packages post/postun install scripts
 set(CPACK_RPM_KHIOPS_CORE_POST_INSTALL_SCRIPT_FILE "${PROJECT_SOURCE_DIR}/packaging/linux/redhat/khiops-core.post")
-set(CPACK_RPM_KHIOPS_POST_INSTALL_SCRIPT_FILE "${PROJECT_SOURCE_DIR}/packaging/linux/redhat/khiops.post")
 set(CPACK_RPM_KNI_POST_INSTALL_SCRIPT_FILE "${PROJECT_SOURCE_DIR}/packaging/linux/redhat/kni.post")
 set(CPACK_RPM_KNI_POSTUN_INSTALL_SCRIPT_FILE "${PROJECT_SOURCE_DIR}/packaging/linux/redhat/kni.postun")
 
