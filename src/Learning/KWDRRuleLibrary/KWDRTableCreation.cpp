fa9179ee360b3db2b7e8da71b1f892d0c30e0a0e
Marc Boullé <marc.boulle@orange.com>
2024-05-22 16:51:44 +0200

    Implement management of table views with derived variables
    Mise en place de classes de service ancetre pour les regle de creation:
    - KWDRRelationCreationRule
      - GetReference: renvoie true
      - Compile: memorisation de la classe cible et des index source cet cible de attributs a copier
      - CheckOperandsCompleteness: debut d'implementation des constraintes pour les aliemntations de type vue
      - IsFirstOperandViewType: methode virtuelle pour indiquer que le premier operande est la source de la vue
      - NewTargetObject: creation d'un objet de type vue (cf. KWObject::GetViewTypeUse)
      - CopyObjectCommonNativeAttributes: recopie optimisee des attributs de vue
    - KWDRTableCreationRule
    - KWDREntityCreationRule
    
    KWDerivationRuleOperand::CheckCompleteness
    - ajout d'une contrainte pour interdire l'utilisation d'une regle de creation de table en operande
    
    KWDRRelationCreationRule::BuildAllUsedAttributes
    - premiere implementation qui marche
    - teste dans LearningTest\TestKhiops\z_TableCreationRules\TableViewErrors avec de nombreux effets de bord
    - ne prend pas en compte une optimisation poussee
      - complexe a mettre en place
      - il faut evaluer quels attributs sources de la vue snt effectivement utilises en fonction des
        attributs cible de la vue utilises par d'autres regles
      - comme une vue peut etre calcule depuis plusizeurs dictionanires sources differents, un meme attrbut cible
        de vue peut avoir une source de natrure distinctes selon le dictionnaire d'origine
      - acctuellement, on intégre systematiquement tous els attributs source de la regle BuidTableView
      - mais on pourrait evaluer de faon fine quels sont les seuls attributs sources vraiement necessaires
    
    Au passage: correction de deux warning dans KDMultiTableFeatureConstruction.cpp

--- src/Learning/KWDRRuleLibrary/KWDRTableCreation.cpp
+++ src/Learning/KWDRRuleLibrary/KWDRTableCreation.cpp
@@ -9,6 +9,357 @@ void KWDRRegisterTableCreationRules()
 	KWDerivationRule::RegisterDerivationRule(new KWDRProtoBuildTableView);
 }
 
+////////////////////////////////////////////////////////////////////////////
+// Classe KWDRRelationCreationRule
+
+KWDRRelationCreationRule::KWDRRelationCreationRule()
+{
+	kwcCompiledTargetClass = NULL;
+}
+
+KWDRRelationCreationRule::~KWDRRelationCreationRule() {}
+boolean KWDRRelationCreationRule::GetReference() const
+{
+	require(KWType::IsRelation(GetType()));
+	return false;
+}
+
+boolean KWDRRelationCreationRule::CheckOperandsCompleteness(const KWClass* kwcOwnerClass) const
+{
+	boolean bOk;
+	KWClass* kwcSourceClass;
+	KWClass* kwcTargetClass;
+	KWAttribute* sourceAttribute;
+	KWAttribute* targetAttribute;
+
+	// Appel de la methode ancetre
+	bOk = KWDerivationRule::CheckOperandsCompleteness(kwcOwnerClass);
+
+	// Verification du premier operande dans le cas d'une alimenattion de type vue
+	if (bOk and IsFirstOperandViewType())
+	{
+		assert(GetOperandNumber() > 0);
+		assert(KWType::IsRelation(GetFirstOperand()->GetType()));
+
+		// Recherche des classes source et cible
+		kwcSourceClass = kwcOwnerClass->GetDomain()->LookupClass(GetFirstOperand()->GetObjectClassName());
+		kwcTargetClass = kwcOwnerClass->GetDomain()->LookupClass(GetObjectClassName());
+		assert(kwcSourceClass != NULL);
+		assert(kwcTargetClass != NULL);
+
+		// Parcours des attributs natif de la classe cible
+		targetAttribute = kwcTargetClass->GetHeadAttribute();
+		while (targetAttribute != NULL)
+		{
+			// On ne traite que les attributs natifs
+			if (targetAttribute->GetAnyDerivationRule() == NULL)
+			{
+				assert(KWType::IsData(targetAttribute->GetType()));
+
+				// Recherche d'un attribut natif source de meme nom
+				sourceAttribute = kwcSourceClass->LookupAttribute(targetAttribute->GetName());
+
+				// Erreur si pas d'attribut correspondant trouve
+				if (sourceAttribute == NULL)
+				{
+					AddError("In output dictionary " + kwcTargetClass->GetName() +
+						 ", no variable found for secondary variable " +
+						 targetAttribute->GetName() + " in input dictionary " +
+						 kwcSourceClass->GetName() + " of first operand");
+					bOk = false;
+				}
+				// Erreur si le type trouve est incorrect
+				else if (sourceAttribute->GetType() != targetAttribute->GetType())
+				{
+					AddError("In output dictionary " + kwcTargetClass->GetName() +
+						 ", variable found with different type (" +
+						 KWType::ToString(targetAttribute->GetType()) + " versus " +
+						 KWType::ToString(sourceAttribute->GetType()) +
+						 ") for secondary variable " + targetAttribute->GetName() +
+						 " in input dictionary " + kwcSourceClass->GetName() +
+						 " of first operand");
+					bOk = false;
+				}
+
+				// Erreur si le type de relation est incorrect
+				if (bOk and KWType::IsRelation(sourceAttribute->GetType()) and
+				    sourceAttribute->GetClass()->GetName() != targetAttribute->GetClass()->GetName())
+				{
+					AddError("In output dictionary " + kwcTargetClass->GetName() +
+						 ", variable found with different type of " +
+						 KWType::ToString(sourceAttribute->GetType()) + " (" +
+						 targetAttribute->GetClass()->GetName() + " versus " +
+						 sourceAttribute->GetClass()->GetName() + ") for secondary variable " +
+						 targetAttribute->GetName() + " in input dictionary " +
+						 kwcSourceClass->GetName() + " of first operand");
+					bOk = false;
+				}
+
+				// Erreur dans le cas d'un bloc si bloc different ou si VarKey different
+				if (bOk and (sourceAttribute->IsInBlock() or targetAttribute->IsInBlock()))
+				{
+					//DDD Pour l'instant, on interdit les attributs cibles dans un bloc
+					if (targetAttribute->IsInBlock())
+					{
+						AddError("In output dictionary " + kwcTargetClass->GetName() +
+							 ", variable " + targetAttribute->GetName() + " in block " +
+							 targetAttribute->GetAttributeBlock()->GetName() +
+							 " not allowed");
+						bOk = false;
+					}
+					//DDD EN COURS
+					// Erreur si nom de bloc ifferent
+
+					// Erreur si type de VarKey different
+
+					// Erreur si VarKey different
+				}
+
+				// Arret si erreur
+				if (not bOk)
+					break;
+			}
+
+			// Attribut suivant
+			kwcTargetClass->GetNextAttribute(targetAttribute);
+		}
+	}
+	return bOk;
+}
+
+void KWDRRelationCreationRule::Compile(KWClass* kwcOwnerClass)
+{
+	const boolean bTrace = false;
+	KWClass* kwcSourceClass;
+	KWAttribute* sourceAttribute;
+	KWAttribute* targetAttribute;
+	int nAttribute;
+
+	require(kwcOwnerClass != NULL);
+
+	// Appel de la methode ancetre
+	KWDerivationRule::Compile(kwcOwnerClass);
+
+	// Compilation dans le cas d'un premier operande utilise pour une alimentation de type vue
+	if (IsFirstOperandViewType())
+	{
+		assert(GetOperandNumber() > 0);
+		assert(KWType::IsRelation(GetFirstOperand()->GetType()));
+
+		// Recherche et memorisation de la classe source
+		kwcCompiledTargetClass = kwcOwnerClass->GetDomain()->LookupClass(GetObjectClassName());
+		assert(kwcCompiledTargetClass != NULL);
+
+		// Recherche de la classe source
+		kwcSourceClass = kwcOwnerClass->GetDomain()->LookupClass(GetFirstOperand()->GetObjectClassName());
+		assert(kwcSourceClass != NULL);
+
+		// Trace initiale
+		if (bTrace)
+		{
+			cout << "Compile rule " << GetName() << "\n";
+			cout << "  Owner dictionary\t" << kwcOwnerClass->GetName() << "\t"
+			     << kwcOwnerClass->GetDomain()->GetName() << "\t" << kwcOwnerClass->GetCompileFreshness()
+			     << "\t" << kwcOwnerClass << "\n";
+			cout << "  Source dictionary\t" << kwcSourceClass->GetName() << "\t"
+			     << kwcSourceClass->GetDomain()->GetName() << "\t" << kwcSourceClass->GetCompileFreshness()
+			     << "\t" << kwcSourceClass << "\n";
+			cout << "  Target dictionary\t" << kwcCompiledTargetClass->GetName() << "\t"
+			     << kwcCompiledTargetClass->GetDomain()->GetName() << "\t"
+			     << kwcCompiledTargetClass->GetCompileFreshness() << "\t" << kwcCompiledTargetClass << "\n";
+			cout << "\tVariable\tType\tSource index\tTarget index\n";
+		}
+
+		// Reinitialisation des resultats de compilation
+		livSourceAttributeLoadIndexes.SetSize(0);
+		livTargetAttributeLoadIndexes.SetSize(0);
+		ivTargetAttributeTypes.SetSize(0);
+
+		// Parcours des attributs natifs de la classe cible charges en memoire
+		for (nAttribute = 0; nAttribute < kwcCompiledTargetClass->GetLoadedDenseAttributeNumber(); nAttribute++)
+		{
+			targetAttribute = kwcCompiledTargetClass->GetLoadedDenseAttributeAt(nAttribute);
+
+			// On ne traite que les attributs natifs utilises
+			if (targetAttribute->GetDerivationRule() == NULL)
+			{
+				assert(KWType::IsStored(targetAttribute->GetType()) or
+				       KWType::IsRelation(targetAttribute->GetType()));
+
+				// Recherche d'un attribut natif source de meme nom
+				sourceAttribute = kwcSourceClass->LookupAttribute(targetAttribute->GetName());
+				assert(sourceAttribute != NULL);
+				assert(sourceAttribute->GetType() == targetAttribute->GetType());
+
+				// Memorisation des infos de chargement si l'attribut source est charge
+				// Il peut ne pas etre charge dans le dictionnaire "logique", mais il sera
+				// de toute facon charge dans le dictionnaire "physique"
+				// Cf. gestion des dictionnaire logiques et physiques dans KWDatabase
+				if (sourceAttribute->GetLoaded())
+				{
+					livSourceAttributeLoadIndexes.Add(sourceAttribute->GetLoadIndex());
+					livTargetAttributeLoadIndexes.Add(targetAttribute->GetLoadIndex());
+					ivTargetAttributeTypes.Add(targetAttribute->GetType());
+
+					// Trace par attribut gere par une alimentation de type vue
+					if (bTrace)
+					{
+						cout << "\t" << sourceAttribute->GetName();
+						cout << "\t" << KWType::ToString(sourceAttribute->GetType());
+						cout << "\t" << sourceAttribute->GetLoadIndex();
+						cout << "\t" << targetAttribute->GetLoadIndex();
+						cout << "\n";
+					}
+				}
+			}
+		}
+	}
+}
+
+void KWDRRelationCreationRule::BuildAllUsedAttributes(const KWAttribute* derivedAttribute,
+						      NumericKeyDictionary* nkdAllUsedAttributes) const
+{
+	KWClass* kwcSourceClass;
+	KWClass* kwcTargetClass;
+	KWAttribute* sourceAttribute;
+	KWAttribute* targetAttribute;
+	KWDerivationRule* sourceAttributeRule;
+
+	require(derivedAttribute != NULL);
+	require(KWType::IsRelation(derivedAttribute->GetType()));
+	require(derivedAttribute->GetClass()->GetName() == GetObjectClassName());
+
+	// Appel de la methode ancetre
+	KWDerivationRule::BuildAllUsedAttributes(derivedAttribute, nkdAllUsedAttributes);
+
+	// Verification du premier operande dans le cas d'une alimentation de type vue
+	if (IsFirstOperandViewType())
+	{
+		assert(GetOperandNumber() > 0);
+		assert(KWType::IsRelation(GetFirstOperand()->GetType()));
+
+		// Recherche des classes source et cible
+		kwcSourceClass = derivedAttribute->GetParentClass()->GetDomain()->LookupClass(
+		    GetFirstOperand()->GetObjectClassName());
+		kwcTargetClass = derivedAttribute->GetClass();
+		assert(kwcSourceClass != NULL);
+		assert(kwcTargetClass != NULL);
+
+		// Parcours des attributs natifs de la classe cible
+		targetAttribute = kwcTargetClass->GetHeadAttribute();
+		while (targetAttribute != NULL)
+		{
+			// On ne traite que les attributs natifs utilises
+			//DDD if (targetAttribute->GetUsed() and targetAttribute->GetDerivationRule() == NULL)
+			if (targetAttribute->GetDerivationRule() == NULL)
+			{
+				assert(KWType::IsData(targetAttribute->GetType()));
+
+				// Recherche d'un attribut natif source de meme nom
+				sourceAttribute = kwcSourceClass->LookupAttribute(targetAttribute->GetName());
+				assert(sourceAttribute != NULL);
+				assert(sourceAttribute->GetType() == targetAttribute->GetType());
+
+				// Analyse de l'attribut si necessaire
+				if (nkdAllUsedAttributes->Lookup(sourceAttribute) == NULL)
+				{
+					// Memorisation de l'attribut dans le dictionnaire
+					nkdAllUsedAttributes->SetAt(sourceAttribute, sourceAttribute);
+
+					// Acces a la regle d'attribut ou de bloc
+					sourceAttributeRule = sourceAttribute->GetAnyDerivationRule();
+					if (sourceAttributeRule != NULL)
+						sourceAttributeRule->BuildAllUsedAttributes(sourceAttribute,
+											    nkdAllUsedAttributes);
+				}
+			}
+
+			// Attribut suivant
+			kwcTargetClass->GetNextAttribute(targetAttribute);
+		}
+	}
+}
+
+boolean KWDRRelationCreationRule::IsFirstOperandViewType() const
+{
+	return true;
+}
+
+void KWDRRelationCreationRule::CopyObjectCommonNativeAttributes(const KWObject* kwoSourceObject,
+								KWObject* kwoTargetObject) const
+{
+	ObjectArray* oaSourceObjectArray;
+	ObjectArray* oaTargetObjectArray;
+	int nAttribute;
+	int nType;
+	KWLoadIndex liSource;
+	KWLoadIndex liTarget;
+
+	require(IsCompiled());
+	require(kwoSourceObject != NULL);
+	require(kwoTargetObject != NULL);
+
+	// Recopie des valeurs des attributs commun
+	for (nAttribute = 0; nAttribute < ivTargetAttributeTypes.GetSize(); nAttribute++)
+	{
+		nType = ivTargetAttributeTypes.GetAt(nAttribute);
+		liSource = livSourceAttributeLoadIndexes.GetAt(nAttribute);
+		liTarget = livTargetAttributeLoadIndexes.GetAt(nAttribute);
+
+		// Recopie de la valeur selon le type
+		switch (nType)
+		{
+		case KWType::Symbol:
+			kwoTargetObject->SetSymbolValueAt(liTarget, kwoSourceObject->ComputeSymbolValueAt(liSource));
+			break;
+		case KWType::Continuous:
+			kwoTargetObject->SetContinuousValueAt(liTarget,
+							      kwoSourceObject->ComputeContinuousValueAt(liSource));
+			break;
+		case KWType::Date:
+			kwoTargetObject->SetDateValueAt(liTarget, kwoSourceObject->ComputeDateValueAt(liSource));
+			break;
+		case KWType::Time:
+			kwoTargetObject->SetTimeValueAt(liTarget, kwoSourceObject->ComputeTimeValueAt(liSource));
+			break;
+		case KWType::Timestamp:
+			kwoTargetObject->SetTimestampValueAt(liTarget,
+							     kwoSourceObject->ComputeTimestampValueAt(liSource));
+			break;
+		case KWType::TimestampTZ:
+			kwoTargetObject->SetTimestampTZValueAt(liTarget,
+							       kwoSourceObject->ComputeTimestampTZValueAt(liSource));
+			break;
+		case KWType::Text:
+			kwoTargetObject->SetTextValueAt(liTarget, kwoSourceObject->ComputeTextValueAt(liSource));
+			break;
+		case KWType::Object:
+			kwoTargetObject->SetObjectValueAt(liTarget, kwoSourceObject->ComputeObjectValueAt(liSource));
+			break;
+		case KWType::ObjectArray:
+			oaSourceObjectArray = kwoSourceObject->ComputeObjectArrayValueAt(liSource);
+			oaTargetObjectArray = NULL;
+			if (oaSourceObjectArray != NULL)
+				oaTargetObjectArray = oaSourceObjectArray->Clone();
+			kwoTargetObject->SetObjectArrayValueAt(liTarget, oaTargetObjectArray);
+			break;
+		default:
+			assert(false);
+			break;
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////
+// Classe KWDRRelationCreationRule
+
+KWDRTableCreationRule::KWDRTableCreationRule()
+{
+	SetType(KWType::ObjectArray);
+}
+
+KWDRTableCreationRule::~KWDRTableCreationRule() {}
+
 //////////////////////////////////////////////////////////////////////////////////////
 // Classe KWDRProtoBuildTableView
 
@@ -16,7 +367,6 @@ KWDRProtoBuildTableView::KWDRProtoBuildTableView()
 {
 	SetName("ProtoBuildTableView");
 	SetLabel("Table view");
-	SetType(KWType::ObjectArray);
 	SetOperandNumber(1);
 	GetFirstOperand()->SetType(KWType::ObjectArray);
 }
@@ -28,18 +378,11 @@ KWDerivationRule* KWDRProtoBuildTableView::Create() const
 	return new KWDRProtoBuildTableView;
 }
 
-boolean KWDRProtoBuildTableView::GetReference() const
-{
-	require(KWType::IsGeneralRelation(GetType()));
-	return false;
-}
-
 ObjectArray* KWDRProtoBuildTableView::ComputeObjectArrayResult(const KWObject* kwoObject) const
 {
 	ObjectArray* oaObjectArrayOperand;
 	KWObject* kwoContainedObject;
 	KWObject* kwoCopiedContainedObject;
-	KWClass* kwcTargetClass;
 	int nObject;
 
 	require(IsCompiled());
@@ -51,78 +394,15 @@ ObjectArray* KWDRProtoBuildTableView::ComputeObjectArrayResult(const KWObject* k
 	oaObjectArrayOperand = GetFirstOperand()->GetObjectArrayValue(kwoObject);
 	if (oaObjectArrayOperand != NULL and oaObjectArrayOperand->GetSize() > 0)
 	{
-		// Recherche de la classe cible
-		kwcTargetClass = kwoObject->GetClass()->GetDomain()->LookupClass(GetObjectClassName());
-
 		// Copie du tableau en entree
 		oaResult.SetSize(oaObjectArrayOperand->GetSize());
 		for (nObject = 0; nObject < oaObjectArrayOperand->GetSize(); nObject++)
 		{
 			kwoContainedObject = cast(KWObject*, oaObjectArrayOperand->GetAt(nObject));
-			kwoCopiedContainedObject = new KWObject(kwcTargetClass, 1);
-			kwoCopiedContainedObject->SetViewTypeUse(true);
-			CopyObjectCommonNativeFields(kwoContainedObject, kwoCopiedContainedObject);
+			kwoCopiedContainedObject = NewTargetObject(nObject + 1);
+			CopyObjectCommonNativeAttributes(kwoContainedObject, kwoCopiedContainedObject);
 			oaResult.SetAt(nObject, kwoCopiedContainedObject);
 		}
 	}
 	return &oaResult;
-}
-
-void KWDRProtoBuildTableView::CopyObjectCommonNativeFields(const KWObject* kwoSourceObject,
-							   KWObject* kwoTargetObject) const
-{
-	const KWClass* kwcSourceClass;
-	const KWClass* kwcTargetClass;
-	const KWAttribute* sourceAttribute;
-	const KWAttribute* targetAttribute;
-	ObjectArray* oaSourceObjectArray;
-	ObjectArray* oaTargetObjectArray;
-	int nAttribute;
-
-	require(kwoSourceObject != NULL);
-	require(kwoTargetObject != NULL);
-
-	// Recopie des valeurs des attributs commun
-	kwcSourceClass = kwoSourceObject->GetClass();
-	kwcTargetClass = kwoTargetObject->GetClass();
-	for (nAttribute = 0; nAttribute < kwcSourceClass->GetLoadedDenseAttributeNumber(); nAttribute++)
-	{
-		sourceAttribute = kwcSourceClass->GetLoadedDenseAttributeAt(nAttribute);
-
-		// Recherche d'un attribut natif cible de meme nom
-		targetAttribute = kwcTargetClass->LookupAttribute(sourceAttribute->GetName());
-
-		// Recopie de la valeur
-		if (targetAttribute != NULL and targetAttribute->GetAnyDerivationRule() == NULL)
-		{
-			assert(targetAttribute->GetType() == sourceAttribute->GetType());
-
-			// Recopie de la valeur selon le type
-			if (sourceAttribute->GetType() == KWType::Continuous)
-				kwoTargetObject->SetContinuousValueAt(
-				    targetAttribute->GetLoadIndex(),
-				    kwoSourceObject->GetContinuousValueAt(sourceAttribute->GetLoadIndex()));
-			else if (sourceAttribute->GetType() == KWType::Symbol)
-				kwoTargetObject->SetSymbolValueAt(
-				    targetAttribute->GetLoadIndex(),
-				    kwoSourceObject->GetSymbolValueAt(sourceAttribute->GetLoadIndex()));
-			else if (sourceAttribute->GetType() == KWType::Object)
-				kwoTargetObject->SetObjectValueAt(
-				    targetAttribute->GetLoadIndex(),
-				    kwoSourceObject->GetObjectValueAt(sourceAttribute->GetLoadIndex()));
-			else if (sourceAttribute->GetType() == KWType::ObjectArray)
-			{
-				oaSourceObjectArray =
-				    kwoSourceObject->GetObjectArrayValueAt(sourceAttribute->GetLoadIndex());
-				oaTargetObjectArray = NULL;
-				if (oaSourceObjectArray != NULL)
-				{
-					oaTargetObjectArray = new ObjectArray;
-					oaTargetObjectArray->CopyFrom(oaSourceObjectArray);
-				}
-				kwoTargetObject->SetObjectArrayValueAt(targetAttribute->GetLoadIndex(),
-								       oaTargetObjectArray);
-			}
-		}
-	}
 }
--- src/Learning/KWDRRuleLibrary/KWDRTableCreation.h
+++ src/Learning/KWDRRuleLibrary/KWDRTableCreation.h
@@ -16,9 +16,118 @@ class KWDRProtoBuildTableView;
 void KWDRRegisterTableCreationRules();
 
 ////////////////////////////////////////////////////////////////////////////
-// Classe KWDRExist
-// Indicateur d'existence presence d'un attribut Object
-class KWDRProtoBuildTableView : public KWDerivationRule
+// Classe KWDRRelationCreationRule
+// Classe ancetre des regle de creation de Table ou Entity,
+// destinee a etre specialisee dans des sous-classes.
+//
+// Dans ce type de regle:
+// - les objets crees sont detruits automatiquement
+// - le type des objet produits en sortie (ObjectClassName) provient
+//   de la declaration de la regle
+// - la regle peut etre utilise uniquement pour definir une variable d'un dictionnaire,
+//   et ne peut etre utilise en operande d'une autre regle
+// - toute variable native (non calculee) des objets en sortie doit etre alimentee
+//   par la regle, selon un des deux mode d'alimentation suivant:
+//   - alimentation de type vue
+//   - alimentation de type calcul
+// - le dictionnaire en sortie peut avoir ses variable utilisées ou non (Used),
+//   et des variable calculee si besoin
+// - un dictionnaire en sortie n'est pas specifique a aux regle de creation de Table:
+//   a priori, un meme dictionnaire pourrait etre utilise pour une variable native alimentee
+//   depuis un fichier, et pour une variable calculee alimentee par une regle de creation
+//
+// Alimentation de type vue:
+// - par defaut, le premier operande de ce type de regle est de type relation, ce qui permet
+// de definir la source de la vue dans les objets en entree
+// - toute variable native en sortie correspondant a une variable en entree de meme nom
+//   est alimentee par la valeur de cette variable
+//   - cette alimentation de type vue est similaire a l'alimentation des objets en memoire
+//     a partir des enregistremenst d'un fichier de donnees, en se basant sur la correspondance
+//     des noms de variables dfans le dictionnaire avec les noms des colonnes dans le fichier
+//   - les variables correspondantes doivent etre de meme type en entree et en sortie
+//   - les variables en sortie peuvent etre alimentee par des variables calculee en entree
+//   - les variable en sortie peuvent etre de type Stored (Numerical, Categorical, Timestamp, Text...)
+//     comme dans les fichier, ou de type Entity ou Table
+//   - on peut avoir des variables calculees en sortie
+//     - les variables calculees en sortie peuvent avoir le meme nom que des variables
+//       en entree, calculees ou non, meme avec des type differents; cela permet de creer
+//       de nouvelles variables independament en entree et en sortie
+//
+// Alimentation de type calcul
+// - une regle doit specifier explicitement dans ces operandes la liste des variables natives en
+//   sortie a alimenter, en utilisant ':' pour separer les operande en entree des operandes en sortie
+// - il peut y avoir plusieurs operandes en sortie, separees par des ','
+// - les operandes en sortie designent explicitement les noms des variables natives a alimenter dans
+//   le dictionnaire en sortie
+//   - cela ne concerne ques les variables natives en sortie du premier niveau dans le cas de variables
+//     de type relation: on ne peut pas specifier l'amilementation d'une variable secondaire d'une
+//     sous-table en sortie
+class KWDRRelationCreationRule : public KWDerivationRule
+{
+public:
+	// Constructeur
+	KWDRRelationCreationRule();
+	~KWDRRelationCreationRule();
+
+	// On indique que la regle cree de nouveau objets
+	boolean GetReference() const override;
+
+	// Verification de la compatibilite des operandes avec la cle de la classe referencee
+	boolean CheckOperandsCompleteness(const KWClass* kwcOwnerClass) const override;
+
+	// Compilation
+	void Compile(KWClass* kwcOwnerClass) override;
+
+	// Construction du dictionnaire de tous les attributs utilises
+	// Redefinition de la methode ancetre pour integrer comme attributs utilises
+	// les attributs de la table source correspond aux attributs de la table en sortie
+	void BuildAllUsedAttributes(const KWAttribute* derivedAttribute,
+				    NumericKeyDictionary* nkdAllUsedAttributes) const override;
+
+	///////////////////////////////////////////////////////
+	///// Implementation
+protected:
+	// Indique si le premier operande est de type Relation pour une alimentation de type vue
+	// Par defaut: true
+	boolean IsFirstOperandViewType() const;
+
+	// Creation d'un objet de la vue avec la classe en sortie
+	KWObject* NewTargetObject(longint lCreationIndex) const;
+
+	// Copie des champs commun d'un object
+	void CopyObjectCommonNativeAttributes(const KWObject* kwoSourceObject, KWObject* kwoTargetObject) const;
+
+	// Index de chargement des attributs source et cible de la vue
+	KWLoadIndexVector livSourceAttributeLoadIndexes;
+	KWLoadIndexVector livTargetAttributeLoadIndexes;
+	IntVector ivTargetAttributeTypes;
+
+	// Classe de la cible de la vue
+	KWClass* kwcCompiledTargetClass;
+};
+
+////////////////////////////////////////////////////////////////////////////
+// Classe KWDRRelationCreationRule
+// Classe ancetre des regle de creation de Table
+class KWDRTableCreationRule : public KWDRRelationCreationRule
+{
+public:
+	// Constructeur
+	KWDRTableCreationRule();
+	~KWDRTableCreationRule();
+
+	///////////////////////////////////////////////////////
+	///// Implementation
+protected:
+	// Object Array en retour de la regle
+	mutable ObjectArray oaResult;
+};
+
+////////////////////////////////////////////////////////////////////////////
+// Classe KWDRProtoBuildTableView
+// Creation d'une vue sur une table
+// Prototype
+class KWDRProtoBuildTableView : public KWDRTableCreationRule
 {
 public:
 	// Constructeur
@@ -28,18 +137,25 @@ public:
 	// Creation
 	KWDerivationRule* Create() const override;
 
-	// On indique que la regle cree de nouveau objets
-	boolean GetReference() const override;
-
 	// Calcul de l'attribut derive
 	ObjectArray* ComputeObjectArrayResult(const KWObject* kwoObject) const override;
 
 	///////////////////////////////////////////////////////
 	///// Implementation
 protected:
-	// Copie des champs commun d'un object
-	void CopyObjectCommonNativeFields(const KWObject* kwoSourceObject, KWObject* kwoTargetObject) const;
-
-	// Object Array en retour de la regle
-	mutable ObjectArray oaResult;
 };
+
+/////////////////////////////////////////////////////////////////
+// Methodes en inline
+
+inline KWObject* KWDRRelationCreationRule::NewTargetObject(longint lCreationIndex) const
+{
+	KWObject* kwoTargetObject;
+
+	require(IsCompiled());
+	require(lCreationIndex >= 1);
+
+	kwoTargetObject = new KWObject(kwcCompiledTargetClass, 1);
+	kwoTargetObject->SetViewTypeUse(true);
+	return kwoTargetObject;
+}
