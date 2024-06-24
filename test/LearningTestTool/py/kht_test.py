012d9e9bbd1b4274d60e71389bafbb7fe0134b38
bruno <bruno.guerraz@orange.com>
2024-06-24 14:36:29 +0200

    In kht_test.py translate openmpi flag in environment variables
    On Linux, kht_test.py will work with OpenMPI and mpich

--- test/LearningTestTool/py/kht_test.py
+++ test/LearningTestTool/py/kht_test.py
@@ -311,14 +311,37 @@ def evaluate_tool_on_test_dir(
                 utils.remove_file(file_path)
 
         # khiops en mode expert via une variable d'environnement
-        os.putenv(kht.KHIOPS_EXPERT_MODE, "true")
+        os.environ[kht.KHIOPS_EXPERT_MODE] = "true"
 
         # khiops en mode HardMemoryLimit via une variable d'environnement pour provoquer
         # un plantage physique de l'allocateur en cas de depassement des contraintes memoires des scenarios
-        os.putenv(kht.KHIOPS_HARD_MEMORY_LIMIT_MODE, "true")
+        os.environ[kht.KHIOPS_HARD_MEMORY_LIMIT_MODE] = "true"
 
         # khiops en mode crash test via une variable d'environnement
-        os.putenv(kht.KHIOPS_CRASH_TEST_MODE, "true")
+        os.environ[kht.KHIOPS_CRASH_TEST_MODE] = "true"
+
+        # Ajout de variables d'environements propres a OpenMPI, elles remplacent les parametres
+        # on peut ansi lancer indiferemment mpich ou openmpi
+        if platform.system() == "Linux":
+            # Supprime les traces en cas d'erreur fatale de khiops. Option --quiet
+            os.environ["OMPI_MCA_orte_execute_quiet"] = "true"
+
+            # permet de lancer plus de processus qu'il n'y a de coeurs. Option --oversubscribe
+            os.environ["OMPI_MCA_rmaps_base_oversubscribe"] = "true"
+
+            # permet de lancer en tant que root. Option --allow-run-as-root
+            os.environ["OMPI_ALLOW_RUN_AS_ROOT"] = "1"
+            os.environ["OMPI_ALLOW_RUN_AS_ROOT_CONFIRM"] = "1"
+
+        # Ajout de variables d'environements propres a OpenMPI, elles remplacent les parametres
+        if platform.system() == "Linux":
+            # Supprime les traces en cas d'erreur fatale de khiops. Option --quiet
+            os.environ["OMPI_MCA_orte_execute_quiet"] = "true"
+            # permet de lancer plus de processus qu'il n'y a de coeurs. Option --oversubscribe
+            os.environ["OMPI_MCA_rmaps_base_oversubscribe"] = "true"
+            # permet de lancer en tant que root. Option --allow-run-as-root
+            os.environ["OMPI_ALLOW_RUN_AS_ROOT"] = "1"
+            os.environ["OMPI_ALLOW_RUN_AS_ROOT_CONFIRM"] = "1"
 
         # Construction des parametres
         khiops_params = []
@@ -331,14 +354,6 @@ def evaluate_tool_on_test_dir(
             if platform.system() == "Darwin":
                 khiops_params.append("-host")
                 khiops_params.append("localhost")
-            # Options specifiques a Open MPI
-            if platform.system() == "Linux":
-                # permet de lancer plus de processus qu'il n'y a de coeurs
-                khiops_params.append("--oversubscribe")
-                # permet de lancer en tant que root
-                khiops_params.append("--allow-run-as-root")
-                # Supprime les traces en cas d'erreur fatale de khiops
-                khiops_params.append("--quiet")
             khiops_params.append("-n")
             khiops_params.append(str(tool_process_number))
         khiops_params.append(tool_exe_path)
