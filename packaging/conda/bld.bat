a8c4d087647e6de007f7d7edb23f84e58ac190b5
bruno <97033386+bruno-at-orange@users.noreply.github.com>
2024-06-25 11:13:32 +0200

    Add conda package for kni and KNITransfer
    * Add conda package for kni and KNITransfer
    
    * In kht_test.py translate openmpi flag in environment variables
    
    On Linux, kht_test.py will work with OpenMPI and mpich

--- packaging/conda/bld.bat
+++ packaging/conda/bld.bat
@@ -3,11 +3,13 @@ REM Echo all output
 
 REM Build the Khiops binaries
 cmake --preset windows-msvc-release -DBUILD_JARS=OFF -DTESTING=OFF
-cmake --build --preset windows-msvc-release --parallel --target MODL MODL_Coclustering
+cmake --build --preset windows-msvc-release --parallel --target MODL MODL_Coclustering KhiopsNativeInterface KNITransfer 
 
 REM Copy the MODL binaries to the Conda PREFIX path
 mkdir %PREFIX%\bin
 copy build\windows-msvc-release\bin\MODL.exe %PREFIX%\bin
 copy build\windows-msvc-release\bin\MODL_Coclustering.exe %PREFIX%\bin
+copy build\windows-msvc-release\bin\KNITransfer.exe %PREFIX%\bin
+copy build\windows-msvc-release\bin\KhiopsNativeInterface.dll %PREFIX%\lib
 
 if errorlevel 1 exit 1
