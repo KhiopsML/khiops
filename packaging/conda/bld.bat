5b1bdeac5c63a12db9ba1f8c4ddc3c192ebd54b5
Popescu V <136721202+popescu-v@users.noreply.github.com>
2024-08-01 12:06:06 +0200

    Specify empty target platform and generator toolset for CMake with Ninja on Windows
    This is because Ninja does not expect target platform and generator
    toolset.
    However, CMake Windows presets set these, which results in Ninja failure.

--- packaging/conda/bld.bat
+++ packaging/conda/bld.bat
@@ -2,7 +2,7 @@ REM Echo all output
 @echo on
 
 REM Build the Khiops binaries
-cmake --preset windows-msvc-release -DBUILD_JARS=OFF -DTESTING=OFF
+cmake --preset windows-msvc-release -DBUILD_JARS=OFF -DTESTING=OFF -A "" -T ""
 cmake --build --preset windows-msvc-release --parallel --target MODL MODL_Coclustering KhiopsNativeInterface KNITransfer 
 
 REM Copy the MODL binaries to the Conda PREFIX path
