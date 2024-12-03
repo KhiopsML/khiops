9d5367aae4e815c439c7a2c792994e4c8c493369
Marc <118964874+marcboulle@users.noreply.github.com>
2024-12-03 11:48:32 +0100

    Use currentDir instead of cwd in launch.vs.json (#478)
    Mise a jour du wiki https://github.com/KhiopsML/khiops/wiki/Setting-Up-the-Development-Environment
    Mise a jour des main de MODL, MODL_Coclustering et KNITransfer en supprimant le code de contournement actuel, en le remplacant par des commentaires

--- src/Learning/KNITransfer/KNITransfer.cpp
+++ src/Learning/KNITransfer/KNITransfer.cpp
@@ -4,40 +4,20 @@
 
 #include "KNITransfer.h"
 
-// Debogage sous Windows Visual C++ 2022 (bug https://github.com/microsoft/vscode-cpptools/issues/8084)
-// Choix en dur du repertoire de lancement
-void SetWindowsDebugDir(const ALString& sDatasetFamily, const ALString& sDataset)
-{
-#ifdef _WIN32
-	ALString sUserRootPath;
-	int nRet;
-
-	// Parametrage du repertoire racine de LearningTest via une variable d'environnement "LearningTestDir"
-	// Il est recommande de positionner cette variable d'environnement via le fichier "launch.vs.json"
-	// du repertoire ".vs", avec la cle "env"
-	// Cf. wiki https://github.com/KhiopsML/khiops/wiki/Setting-Up-the-Development-Environment
-	sUserRootPath = p_getenv("LearningTestDir");
-	sUserRootPath += "/TestKNI/";
-
-	// Pour permettre de continuer a utiliser LearningTest, on ne fait rien s'il y a deja un fichier test.prm
-	// dans le repertoire courant
-	if (FileService::FileExists("test.prm"))
-		return;
-
-	// Changement de repertoire, uniquement pour Windows
-	nRet = _chdir(sUserRootPath + sDatasetFamily + "/" + sDataset);
-	assert(nRet == 0);
-#endif
-}
+// Parametrage des variables d'environnement pour les jeux de test de LearningTest (via .vs/launch.vs.json sous Windows Visual C++)
+// KHIOPS_API_MODE="true"
+// - pour que les chemins relatifs soient traites par rapport au cwd (current working directory) pour le stockage des rapports
+// - sinon, en mode GUI, les chemins relatifs sont geres par rapport au repertoire contenant le fichier de donnees
+// KhiopsFastExitMode="false"
+// - pour que l'on puisse exploiter un scenario de test integralement, meme en cas d'erreurs multiples
+// - sinon, on sort du scenario apres la premiere erreur
 
 int main(int argc, char** argv)
 {
 	KNITransferProject learningProject;
 
+	// Point d'arret sur l'allocation d'un bloc memoire
 	// MemSetAllocIndexExit(1290133);
-
-	// Choix du repertoire de lancement pour le debugage sous Windows (a commenter apres fin du debug)
-	// SetWindowsDebugDir("Standard", "Iris");
 
 	// Lancement du projet
 	learningProject.Start(argc, argv);
