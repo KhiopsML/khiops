50062ccf6a6f63d6a3c18a7eb4ea19de2668345b
Marc Boullé <marc.boulle@orange.com>
2024-12-10 16:57:36 +0100

    Fix bug in multi-table schema with secondary tables referencing their root table
    Contexte
    - cas d'un schema de type graphe, comme les molecules, avec les atomes et les liaisons referencant leur molecule via une une reference
      a la table des molecules qui est Root, ce qui permet de creer les liens entre atomes et liaisons et reciproquement
    - si on met Molecule en dictionnaire d'analyse, cela se passe bien, et on a le schema multi-table avec les data paths suivants
      - Molecule (table principale, qui est Root)
      - Atoms
      - Bonds
    
    Probleme
    - si on met par exemple Atom en dictionnaire d'analyse, on obtient un schema "pathologique"
      - Atoms (table principale des atomes)
      - /Molecule
      - /Molecule/Atoms
      - /Molecule/Bonds
    - le schema des molecule est traite comme une table externe, avec sa sous-tables des atomes, deja utilisee en table principale
      - le fichier des atomes ne peut etre utilise deux fois
      - on a toutes la base des molecules chargee en memoire en tant que table externe
      - ce comportement n'a pas de sens
    
    Correction dans KWMTDatabase
    - probleme documente dans le comemntaire de la nouvelle variable svUnusedRootDictionaryWarnings
      // Warnings pour le cas des tables externes non utilises, gardes pour generer des warnings lors du Check
      //
      // Ce cas arrive si la table principale est une table secondaire d'une table externe qu'elle reference.
      // Par exemple, dans le cas d'un schema de molecules avec des atomes et des liaisons, les atomes et les liaisons
      // referencent leur molecule pour pouvoir recreer dynamiquement le graphe de la structure de la molecule, avec chaque
      // liaison referencant ses atomes extremites et chaque atome referencant ses liaisons adjacentes.
      // Si on choisit les atomes en table d'analyse principale, on doit couper le lien avec la table des molecules
      // pour eviter les cycles dans les donnees
    - UpdateMultiTableMappings
      - identification des dictionnaire non utilisables
      - on ne les prend plus en compte dans le mapping
      - on conserve trace des probleme via un vecteur de warning
    - CheckPartially
      - emission des warnings au moment du Check

--- src/Learning/KWData/KWMTDatabase.h
+++ src/Learning/KWData/KWMTDatabase.h
@@ -125,8 +125,8 @@ protected:
 	void CollectPhysicalStatsMessages(ObjectArray* oaPhysicalMessages) override;
 
 	// Lecture apres la fin de la base, pour detecter d'eventuelles erreurs
-	// Necessaire par exemple en multi-tables pour detecter les eventuelles erreurs (ligne orphelines, probleme de
-	// tri...)
+	// Necessaire par exemple en multi-tables pour detecter les eventuelles erreurs
+	// (ligne orphelines, probleme de tri...)
 	void PhysicalReadAfterEndOfDatabase();
 
 	// Destruction de la classe physique
@@ -145,9 +145,10 @@ protected:
 	// Creation recursive d'un mapping
 	// Le mapping est chaine avec ses mappings composant.
 	// Le tableaux mapping exhaustifs est egalement egalement mis a jour
-	// Les classes referencees sont memorisees dans un dictionnaire, pour gerer les mappings externes a creer
-	// ulterieurement Les mappings crees recursivement sont memorises dans un tableau
-	// Les classes crees analysees sont egalement memorisees dans un dictionnaire, pour eviter des analyse multiples
+	// Les classes referencees sont memorisees dans un dictionnaire et un tableau,
+	// pour gerer les mappings externes a creer ulterieurement
+	// Les mappings crees recursivement sont memorises dans un tableau
+	// Les classes crees analysees sont egalement memorisees dans un dictionnaire, pour eviter des analyses multiples
 	KWMTDatabaseMapping* CreateMapping(ObjectDictionary* odReferenceClasses, ObjectArray* oaRankedReferenceClasses,
 					   ObjectDictionary* odAnalysedCreatedClasses, KWClass* mappedClass,
 					   boolean bIsExternalTable, const ALString& sOriginClassName,
@@ -157,7 +158,7 @@ protected:
 	// Gestion des objets natifs references
 	// Les objets natifs references sont les objets (principaux, et non composants)
 	// appartenant aux classes racines des tables secondaires.
-	// Leurs mappings racines sont definis dans le tableau oaRootRefTableMappings.
+	// Leurs mappings racines sont definis dans le tableau oaRootReferenceTableMappings.
 	// A l'ouverture de la base, tous les objets racines referenceables sont charges en memoire
 	// dans leur representation physique, et stockees dans une base memoire, permettant
 	// de rechercher efficacement un objet d'une classe donnee par sa cle.
@@ -213,6 +214,9 @@ protected:
 	KWObject* DMTMPhysicalRead(KWMTDatabaseMapping* mapping);
 	void DMTMPhysicalWrite(KWMTDatabaseMapping* mapping, const KWObject* kwoObject);
 
+	// Affichage d'un tableau de mapping
+	void WriteMapingArray(ostream& ost, const ALString& sTitle, const ObjectArray* oaMappings) const;
+
 	/////////////////////////////////////////////////
 	// Attributs de l'implementation
 
@@ -222,14 +226,23 @@ protected:
 	// et tables secondaires
 	mutable KWMTDatabaseMapping* mainMultiTableMapping;
 
-	// Mapping racine des tables secondaires (reference a un sous-ensemble des mappings du tableau
-	// oaMultiTableMappings)
-	mutable ObjectArray oaRootRefTableMappings;
+	// Mapping racine des tables externes (sous-ensemble des mappings du tableau oaMultiTableMappings)
+	mutable ObjectArray oaRootReferenceTableMappings;
 
 	// Mapping multi-tables: tableau exhaustif de tous les mappings (permet une interface d'edition "a plat")
 	// Attention, le mapping de la classe principale est toujours integree comme premier element du tableau,
 	// et ne doit toujours etre synchronise
 	mutable ObjectArray oaMultiTableMappings;
+
+	// Warnings pour le cas des tables externes non utilises, gardes pour generer des warnings lors du Check
+	//
+	// Ce cas arrive si la table principale est une table secondaire d'une table externe qu'elle reference.
+	// Par exemple, dans le cas d'un schema de molecules avec des atomes et des liaisons, les atomes et les liaisons
+	// referencent leur molecule pour pouvoir recreer dynamiquement le graphe de la structure de la molecule, avec chaque
+	// liaison referencant ses atomes extremites et chaque atome referencant ses liaisons adjacentes.
+	// Si on choisit les atomes en table d'analyse principale, on doit couper le lien avec la table des molecules
+	// pour eviter les cycles dans les donnees
+	mutable StringVector svUnusedRootDictionaryWarnings;
 
 	// Nombre d'enregistrements ayant ete sautes (Skip)
 	// On ne peut faire des tests d'integrite sur les lignes de tables secondaires non associees
