5fcb2fe77c599d206aa61cf1478eba568b728cfc
Marc Boullé <marc.boulle@orange.com>
2025-01-02 13:47:52 +0100

    Backport update copyright notices to 2025 in all source files
    Backport du second commit de la PR https://github.com/KhiopsML/khiops/pull/506

--- scripts/update-copyright.py
+++ scripts/update-copyright.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 Orange. All rights reserved.
+# Copyright (c) 2023-2025 Orange. All rights reserved.
 # This software is distributed under the BSD 3-Clause-clear License, the text of which is available
 # at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
 
@@ -16,7 +16,7 @@ from datetime import datetime
 byte_linesep = bytes(os.linesep, encoding="ascii")
 copyright_banner_lines = [
     bytes(
-        f"Copyright (c) 2024 Orange. All rights reserved.",
+        f"Copyright (c) 2023-{datetime.today().year} Orange. All rights reserved.",
         encoding="ascii",
     ),
     b"This software is distributed under the BSD 3-Clause-clear License, the text of which is available",
