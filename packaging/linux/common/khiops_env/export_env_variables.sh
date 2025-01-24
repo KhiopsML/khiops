3760d03a11fd6516eee284248d60f54e036de0ab
bruno <97033386+bruno-at-orange@users.noreply.github.com>
2025-01-24 08:32:22 +0100

    Update khiops_env: remove -hostfile and -n flags (#540)
    * Changes made to improve the usability on K8s:
    
    - remove the KHIOPS_HOST_FILE: the host flag is not mandatory, it can be replaced by mpi environment variables
    - by default, the -n flag is removed for openmpi, it will be easier to use on K8s
    
    * Refactoring
    
    we use _MPIEXEC to set the path of mpiexec (different on conda and system wide)
    And after all tests, we set KHIOPS_MPI_COMMAND. In tha way, on conda or not we're sure to have the same command line

--- packaging/linux/common/khiops_env/export_env_variables.sh
+++
@@ -1,6 +0,0 @@
-
-# Export environment variables to MPI processes
-for line in $(env | grep -E '^(KHIOPS|Khiops|AWS_|S3_|GOOGLE_)'); do
-    name=${line%%=*}
-    MPI_EXTRA_FLAGS="${MPI_EXTRA_FLAGS} -x ${name}"
-done
