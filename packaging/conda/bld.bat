50a1c363d2afc19670f66d2ba265b0b977884a8b
Popescu V <136721202+popescu-v@users.noreply.github.com>
2025-11-25 13:25:01 +0100

    Build Windows Conda package as on conda-forge
    Thus, we copy the executables and expect the DLLs in the Library\bin
    directory.
    
    This is useful for quick testing of the Conda packages.

--- packaging/conda/bld.bat
+++ packaging/conda/bld.bat
@@ -5,24 +5,22 @@ REM Build the Khiops binaries
 cmake -B build\conda -S . -D BUILD_JARS=OFF -D TESTING=OFF -D CMAKE_BUILD_TYPE=Release -G Ninja
 cmake --build build\conda --parallel --target MODL MODL_Coclustering KhiopsNativeInterface KNITransfer _khiopsgetprocnumber
 
-mkdir %PREFIX%\bin
-
 REM Copy the khiops-core binaries to the Conda PREFIX path: MODL, MODL_Cocluetsring and _khiopsgetprocnumber.
 REM This last one is used by khiops_env to get the physical cores number 
-copy build\conda\bin\MODL.exe %PREFIX%\bin
-copy build\conda\bin\MODL_Coclustering.exe %PREFIX%\bin
-copy build\conda\bin\_khiopsgetprocnumber.exe %PREFIX%\bin
+copy build\conda\bin\MODL.exe %LIBRARY_BIN%
+copy build\conda\bin\MODL_Coclustering.exe %LIBRARY_BIN%
+copy build\conda\bin\_khiopsgetprocnumber.exe %LIBRARY_BIN%
 
 REM Copy the KNITransfer for the kni-transfer package (a test package for kni)
-copy build\conda\bin\KNITransfer.exe %PREFIX%\bin
+copy build\conda\bin\KNITransfer.exe %LIBRARY_BIN%
 
 REM Copy the KhiopsNativeInterface libs for the kni package
-copy build\conda\bin\KhiopsNativeInterface.dll %PREFIX%\bin
-copy build\conda\lib\KhiopsNativeInterface.lib %PREFIX%\lib
+copy build\conda\bin\KhiopsNativeInterface.dll %LIBRARY_BIN%
+copy build\conda\lib\KhiopsNativeInterface.lib %LIBRARY_LIB%
 
 REM Copy the scripts to the Conda PREFIX path
-copy build\conda\tmp\khiops_env.cmd %PREFIX%\bin
-copy build\conda\tmp\khiops_coclustering.cmd %PREFIX%\bin
-copy build\conda\tmp\khiops.cmd %PREFIX%\bin
+copy build\conda\tmp\khiops_env.cmd %LIBRARY_BIN%
+copy build\conda\tmp\khiops_coclustering.cmd %LIBRARY_BIN%
+copy build\conda\tmp\khiops.cmd %LIBRARY_BIN%
 
 if errorlevel 1 exit 1
