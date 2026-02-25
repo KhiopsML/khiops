0cbfaaf71df4e5e86b5ceccab42f0477b53c5c43
Popescu V <136721202+popescu-v@users.noreply.github.com>
2026-02-25 16:50:06 +0100

    Use TAB instead of 4 spaces on the C++ file

--- src/Parallel/PLSamples/compute_pi/compute_pi.cpp
+++ src/Parallel/PLSamples/compute_pi/compute_pi.cpp
@@ -11,48 +11,48 @@
 
 void UseMPI()
 {
-    #if defined USE_MPI
-        // Mise en place du driver parallel
-        PLParallelTask::SetDriver(PLMPITaskDriver::GetDriver());
+	#if defined USE_MPI
+		// Mise en place du driver parallel
+		PLParallelTask::SetDriver(PLMPITaskDriver::GetDriver());
 
-        // Initialisation des ressources systeme
-        PLParallelTask::GetDriver()->InitializeResourceSystem();
+		// Initialisation des ressources systeme
+		PLParallelTask::GetDriver()->InitializeResourceSystem();
 
-        // Chargement du driver pour l'acces aux fichiers distants (file://)
-        if (RMResourceManager::GetResourceSystem()->GetHostNumber() > 1 or PLTaskDriver::GetFileServerOnSingleHost())
-            SystemFileDriverCreator::RegisterDriver(new PLMPISystemFileDriverRemote);
+		// Chargement du driver pour l'acces aux fichiers distants (file://)
+		if (RMResourceManager::GetResourceSystem()->GetHostNumber() > 1 or PLTaskDriver::GetFileServerOnSingleHost())
+			SystemFileDriverCreator::RegisterDriver(new PLMPISystemFileDriverRemote);
 
-        // Verification des versions de chaque processus
-        PLParallelTask::SetVersion("1.0");
-        PLMPITaskDriver::CheckVersion();
-    #endif // USE_MPI
+		// Verification des versions de chaque processus
+		PLParallelTask::SetVersion("1.0");
+		PLMPITaskDriver::CheckVersion();
+	#endif // USE_MPI
 }
 
 int main(int argc, char** argv)
 {
-    UseMPI();
-    boolean bIsSimulatedParallel = StringToBoolean(argv[1]);
-    int nTotalIter = 100;
-    int nWorkerNumber = 4;
-    PLComputePiTask* plComputePiTask = new PLComputePiTask();
-    PLParallelTask::RegisterTask(plComputePiTask);
-    PLParallelTask::SetParallelSimulated(bIsSimulatedParallel);
-    plComputePiTask->SetTotalIteration(nTotalIter);
-    PLParallelTask::SetSimulatedSlaveNumber(nWorkerNumber);
+	// UseMPI();
+	boolean bIsSimulatedParallel = StringToBoolean(argv[1]);
+	int nTotalIter = 100;
+	int nWorkerNumber = 4;
+	PLComputePiTask* plComputePiTask = new PLComputePiTask();
+	PLParallelTask::RegisterTask(plComputePiTask);
+	PLParallelTask::SetParallelSimulated(bIsSimulatedParallel);
+	plComputePiTask->SetTotalIteration(nTotalIter);
+	PLParallelTask::SetSimulatedSlaveNumber(nWorkerNumber);
 	SystemFileDriverCreator::RegisterExternalDrivers();
-    if (PLParallelTask::IsMasterProcess())
-    {
-        cout << "Parallel mode availability is: " << PLParallelTask::GetDriver()->IsParallelModeAvailable() << endl;
+	if (PLParallelTask::IsMasterProcess())
+	{
+		cout << "Parallel mode availability is: " << PLParallelTask::GetDriver()->IsParallelModeAvailable() << endl;
 		FileService::SetApplicationName("compute_pi");
-        plComputePiTask->ComputePi();
-        cout << "Computed Pi (parallel): " << plComputePiTask->GetComputedPi() << endl;
-        PLParallelTask::GetDriver()->StopSlaves();
-    }
-    else
-    {
-        // Lancement de l'esclave
-        PLParallelTask::GetDriver()->StartSlave();
-    }
-    SystemFileDriverCreator::UnregisterDrivers();
-    PLParallelTask::DeleteAllTasks();
+		plComputePiTask->ComputePi();
+		cout << "Computed Pi (parallel): " << plComputePiTask->GetComputedPi() << endl;
+		PLParallelTask::GetDriver()->StopSlaves();
+	}
+	else
+	{
+		// Lancement de l'esclave
+		PLParallelTask::GetDriver()->StartSlave();
+	}
+	SystemFileDriverCreator::UnregisterDrivers();
+	PLParallelTask::DeleteAllTasks();
 }
