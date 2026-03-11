f9b0f167ca8b41199d2ae78249ca39b3e7a0e3ff
Popescu V <136721202+popescu-v@users.noreply.github.com>
2026-03-11 19:45:34 +0100

    Finalize the implementation of the parallel token collection task
    - enable the slaves to take into account in the second pass the specific tokens
      as collected in the first pass
    - enable the master to aggregate the tokens collected by the slaves
    - finalize the token export in the task "driver" method
      `InternalCollectTokenSamples`
    - fix typos and pdate formatting in the comments

--- src/Learning/KDDomainKnowledge/KDTextTokenSampleCollectionTask.h
+++ src/Learning/KDDomainKnowledge/KDTextTokenSampleCollectionTask.h
@@ -16,7 +16,7 @@ class KDTextTokenSampleCollectionTask;
 // Classe KDTextTokenSampleCollectionTask
 // Classe technique utilisee par la classe KDTextFeatureConstruction
 //
-// Analyse de la base pour extraire les tokesn les plus frequent par variable de type texte
+// Analyse de la base pour extraire les tokens les plus frequents par variable de type texte
 class KDTextTokenSampleCollectionTask : public KWDatabaseTask
 {
 public:
@@ -25,15 +25,17 @@ public:
 	~KDTextTokenSampleCollectionTask();
 
 	// Lecture de la base pour collecter un echantillon de tokens les plus frequents par variable secondaire de type
-	// texte Entree: 	 . sourceDatabase: base a analyser, correctement parametree avec uniquement des
-	// variables de type texte (Text ou TextList)
-	//     en used dans la classe principale
-	//   . ivTokenNumbers: nombre de tokens a extraire par variable de type texte, dans le meme ordre que les
-	//   variables de type texte
+	// texte
+	// Entree:
+	// 	. sourceDatabase: base a analyser, correctement parametree avec uniquement des variables de type texte (Text ou TextList)
+	// en used dans la classe principale
+	// 	. ivTokenNumbers: nombre de tokens a extraire par variable de type texte, dans le meme ordre que les variables de type texte
 	// Sortie:
-	// 	 . oaCollectedTokenSamples: tableau de samples de token (ObjectArray de KWTokenFrequency), dans le meme
-	// ordre que les variables de type texte Methode interruptible, retourne false si erreur ou interruption (avec
-	// message), true sinon Memoire: le tableau en sortie appartient a l'appelant, et doit etre initialement vide.
+	// 	. oaCollectedTokenSamples: tableau de samples de tokens (ObjectArray de KWTokenFrequency), dans le meme ordre que les variables de type texte
+	// Methode interruptible, retourne false si erreur ou interruption (avec
+	// message), true sinon
+	// Memoire: le tableau en sortie appartient a l'appelant, et doit etre initialise a la bonne
+	// taille, avec un tableau vide prepare par variable de type texte pour memoriser les tokens collectes.
 	// Les tableaux en sortie et leur contenu en tokens appartiennent a l'appele
 	boolean CollectTokenSamples(const KWDatabase* sourceDatabase, const IntVector* ivTokenNumbers,
 				    ObjectArray* oaCollectedTokenSamples);
@@ -53,8 +55,8 @@ protected:
 	// A terme, cette implementation sera supprimee
 
 	// Prototype basique de la methode principale
-	// Construit un resultat conforme au specification avec des tokens "synthetiques", sans aucune analyse de la
-	// base Permet de tester l'integration basique de la classe
+	// Construit un resultat conforme aux specifications avec des tokens "synthetiques", sans aucune analyse de la base
+	// Permet de tester l'integration basique de la classe
 	boolean DummyCollectTokenSamples(const KWDatabase* sourceDatabase);
 
 	// Implementation en sequentiel, sans dimensionnement et sans passer par les methode de la tache
@@ -67,10 +69,10 @@ protected:
 	// Analyse d'un objet pour en extraire les tokens a l'aide de tokenizer par attribut de type texte
 	boolean AnalyseDatabaseObject(const KWObject* kwoObject, ObjectArray* oaTextTokenizers);
 
-	// Calcul du nombre de token a collecter en mode flux pour assurer la stabilite des resultats
-	// En mode flux, on fait une premier passe avec ce nombre de tokens pour avoir les tokens les
+	// Calcul du nombre de tokens a collecter en mode flux pour assurer la stabilite des resultats
+	// En mode flux, on fait une premiere passe avec ce nombre de tokens pour avoir les tokens les
 	// plus frequents, mais avec une incertitude sur les comptes
-	// On effectue alors une second passe avec les tokens ainsi identifie, et on ne garde que
+	// On effectue alors une seconde passe avec les tokens ainsi identifies, et on ne garde que
 	// les plus frequents selon le nombre demande. Meme s'il n'y a pas de garantie theorique,
 	// cela assure en pratique la stabilite des resultats dans la plupart des cas
 	int GetMaxStreamCollectedTokenNumber(int nRequestedTokenNumber) const;
@@ -78,40 +80,40 @@ protected:
 	//////////////////////////////////////////////////////////////////////////////////////////
 	// Implementation de la collecte des tokens en parallele
 	// Deux passes sont necessaire
-	//   - la premiere passe collecte en flux un echantillons de tokens parmi les plus frequents
-	//     pour gerer l'incertitude kiee aunstream, on collecte plus de tokens que demande
-	//   - la deusieme passe prend ces tokens en entree et calcule de facon exacte leur effectif
-	//     ce qui permet de renvoyer la sous partie des tokens les plus freqntes, tries
+	//   - la premiere passe collecte en flux un echantillon de tokens parmi les plus frequents
+	//     pour gerer l'incertitude liee aux streams, on collecte plus de tokens que demande
+	//   - la deuxieme passe prend ces tokens en entree et calcule de facon exacte leur effectif
+	//     ce qui permet de renvoyer la sous-partie des tokens les plus freqntes, tries
 	//     par effectif decroissant
 	// Chaque passe est executee en parallele, la premiere puis la seconde
-	// Les deux passe etant basees essentiellement sur la tokenisation des textes sont tres proche.
+	// Les deux passes etant basees essentiellement sur la tokenisation des textes sont tres proches.
 	// On choisit d'implementer ces deux taches dans la meme classe pour factoriser le code, en jouant
-	// sur des variante dun parametregae en entree et en sortie.
+	// sur des variantes d'un parametrage en entree et en sortie.
 
 	// Pilotage de l'ensemble des deux passes
 	boolean InternalCollectTokenSamples(const KWDatabase* sourceDatabase);
 
-	// Verification de la coherence des parametres de pilotage des deuxn passes
+	// Verification de la coherence des parametres de pilotage des deux passes
 	boolean CheckPassParameters() const;
 
 	//////////////////////////////////////////////////////////////////////////////////////////
-	// Gestion des indicateur de performances
+	// Gestion des indicateurs de performance
 	// Cela permet d'afficher la performance en memoire et temps de calcul le temps de la mise
-	// au point des algorithme
+	// au point des algorithmes
 
 	// Debut de la collecte des indicateurs de performance
-	// On indique si on souhaite les afficher effectivement par la syuite
+	// On indique si on souhaite les afficher effectivement par la suite
 	void StartCollectPerformanceIndicators(boolean bDisplay);
 
 	// Fin de la collecte des indicateurs
 	void StopCollectPerformanceIndicators();
 
-	// Affichage des indicateurs collectes, en associant un livelle ainsi qu'un tableau de tokenizer
+	// Affichage des indicateurs collectes, en associant un libelle ainsi qu'un tableau de tokenizers
 	// pour afficher le nombre de tokens collectes
 	void DisplayPerformanceIndicators(const ALString& sLabel, const ObjectArray* oaTokenizers);
 
 	//////////////////////////////////////////////////////////////////////////////////////////
-	// Implementation des methodes de KWDatabaseTask pour paralleliser la collecter des tokens
+	// Implementation des methodes de KWDatabaseTask pour paralleliser la collecte des tokens
 
 	// Reimplementation des methodes virtuelles de tache
 	const ALString GetTaskName() const override;
@@ -132,24 +134,24 @@ protected:
 	// Type de token
 	PLShared_String shared_sTextFeatures;
 
-	// Indicateur pour savoir si n est dans la premiere passe
+	// Indicateur pour savoir si on est dans la premiere passe
 	PLShared_Boolean shared_bIsFirstPass;
 
-	// Activation du calcul des effectif exact des tokens, c'est a dire qu'une deuxieme passe est necessaire
+	// Activation du calcul des effectifs exacts des tokens, c'est a dire qu'une deuxieme passe est necessaire
 	PLShared_Boolean shared_bComputeExactTokenFrequencies;
 
 	// Vecteur des nombres de tokens par texte a collecter, pour la premiere passe
 	PLShared_IntVector shared_ivFirstPassTokenNumbers;
 
-	// Tableau des tokens specifique dont il faut calcul l'effectif, pour la deuxieme passe
+	// Tableau des tokens specifiques dont il faut calcul l'effectif, pour la deuxieme passe
 	PLShared_ObjectArray* shared_oaSecondPassSpecificTokens;
 
 	//////////////////////////////////////////////////////
-	// Parametre en entree et sortie des esclaves
+	// Parametres en entree et sortie des esclaves
 
-	// Tableau des tokens en sortie d'un esclaves
+	// Tableau des tokens en sortie d'un esclave
 	// Dans la premiere passe, il s'agit d'un echantillon de tokens collecte par l'esclave
-	// Dans le seconde passe, il s'agit des token dont il faut calculer l'effectif, avec l'effectif
+	// Dans le seconde passe, il s'agit des tokens dont il faut calculer l'effectif, avec l'effectif
 	// calcule localement par l'esclave
 	PLShared_ObjectArray* output_oaTokens;
 
@@ -159,7 +161,7 @@ protected:
 	// Nombre de tokens a extraire par variable de typen texte, copie du parametre principal en entree
 	const IntVector* ivMasterTokenNumbers;
 
-	// Tableau de samples de topkesn extraits par variable de type txete, copie du parametre principale en sortie
+	// Tableau de samples de tokens extraits par variable de type texte, copie du parametre principal en sortie
 	// En fin de premiere passe, contient l'echantillon de tokens collectes
 	// En fin de deuxieme passe, contient les tokens collectes avec leur effectif exact
 	ObjectArray* oaMasterCollectedTokenSamples;
