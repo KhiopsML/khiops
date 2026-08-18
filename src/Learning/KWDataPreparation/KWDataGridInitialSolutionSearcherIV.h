645918931306833c0dc47e54008023e27212b365
Marc Boullé <marc.boulle@orange.com>
2026-08-18 10:48:11 +0200

    Build coclustering IxV initial solution using bivariate analysis
    Objectif
    ========
    - cree un coclustering IxV exploitant au mieux les analyses bivariees des variables internes
    - ce coclustering initial servira de point de depart pour optimiser une solution initiale de qualite
    
    Principaux impacts
    =================
    Implementation modulaire via une nouvele classe dediee
    
    Nouvelle classe dediee a cette recherche:
    - KWDataGridInitialSolutionSearcherIV
      - SearchInitialSolution: methode principale
        - prend en entree la grille initiale la plus fine
        - calcul des statistiques bivariees pour les variables internes
        - pour chaque attribut interne, determine le partiton la plus fine issue de l'intersection de toutes
          les partition bivariee impliquee
        - cree en sortie une grille de coclustering avec:
          - des partition des variable internes issue de cette analyse bivariee
          - un cluster de partie de variable singleton par partie de variable obtenue
          - un cluster d'identifiant par ensemble d'identifiant exploitant exactement les memes parties de variables
    
    Utilisation de cette classe dans
    - KWDataGridOptimizerIxV
      - InternalOptimizeDataGrid: appel de BuildInitialSolution avant l'optimisation standard
      - BuildInitialSolution: recherche d'une bonne soution initiale via KWDataGridInitialSolutionSearcherIV,
        puis optimisation de cette solution initiale
    
    Impacts detailles
    =================
    
    - KWDataGridInitialSolutionSearcherIV
      - SearchInitialSolution:
        - exploitation des anlyses bivariees pour calculer les intersection des partitions de chaque attribut
          implique dans une paire
      - Set|GetLearningSpec
      - ComputeInternalAttributesBivariateStats: calcul des statistique bivarie en passant par un classStats de travail
      - GetInternalAttributesBivariateStats
      - CleanInternalAttributesBivariateStats
      - ComputeIntersectionDiscretizations: calcul de l'intersection de discretisations, renvoie un KWDGSAttributeDiscretization
      - ComputeIntersectionGroupings: calcul de l'intersection de groupements de valeurs, renvoie un KWDGSAttributeGrouping
      - WriteJSONAnalysisReport: ecriture du rapport d'analyse bivarie
    - KWValueSignature: classe technique permettant de calculer l'intersection de plusieurs partitions de valeurs
    
    - KWDataGridManager
      - ExportDataGridWithPartitionnedInnerAttributes:
        - etape 1:
          - creation des clusters de partiex de variable selon les specifications de partitionnement
          - on garde des clusters d'identifiants singletons
          - on exporte les cellules, pour s'en servir comme de signatures de chaque identifiant sur les parties de variable
        - etape 2:
          - on identifie les clusters d'identifiant, ayant les meme signatures sur les clusters de parties de variables
        - etape 3
          - on re-cree les clusters d'identifiant selon la partition obtenue grace au signatures
          - on re-exporte les celules pour avoir la grille initiale correcte
      - CreatePartitionnedInnerAttributes
      - KWDataGridManagerCompareIdentifierParts:
        - methode de comparaison de deux parties singletons de type attribut identifier
        - egalite si les cellules des deux parties utilisent les memes parties de variables
      - InitialiseContinuousAttributePartsFromDiscretisation
      - InitialiseSymbolAttributePartsFromGrouping
    
    Autre impacts
    - KWClassStats
      - ComputeStats; protection contre l'absence de spec sur les paires a analyser
      - methodes d'acces au tabeau d'attributs prepares desormais en const
    - KWDataGridOptimizer
      - Set|GetLearningSpec
    - CCLearningProblem
      - BuildCoclustering: parametrage des LearningSpec par SetInitialAttributeNumber

---
+++ src/Learning/KWDataPreparation/KWDataGridInitialSolutionSearcherIV.h
@@ -0,0 +1,134 @@
+// Copyright (c) 2023-2026 Orange. All rights reserved.
+// This software is distributed under the BSD 3-Clause-clear License, the text of which is available
+// at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
+
+#pragma once
+
+class KWDataGridInitialSolutionSearcherIV;
+
+#include "KWClassStats.h"
+#include "KWDataGrid.h"
+#include "KWDataGridCosts.h"
+#include "KWDataGridMerger.h"
+#include "KWDataGridManager.h"
+#include "KWAttributeSubsetStats.h"
+#include "KWTupleTable.h"
+#include "KWLearningSpec.h"
+
+//////////////////////////////////////////////////////////////////////////////////
+// Classe KWDataGridInitialSolutionSearcherIV
+// Recherche d'une solution initiale de qualite pour une coclustering IxV
+class KWDataGridInitialSolutionSearcherIV : public Object
+{
+public:
+	// Constructeur
+	KWDataGridInitialSolutionSearcherIV();
+	~KWDataGridInitialSolutionSearcherIV();
+
+	// Parametrage par les specifications d'apprentissage
+	// Memoire: les specifications sont referencees uniquement
+	void SetLearningSpec(KWLearningSpec* specification);
+	KWLearningSpec* GetLearningSpec() const;
+
+	// Recherche d'une solution initiale meilleure que celle du modele null
+	// Le parametre initialDataGridSolution en sortie contient une grille initiale fine
+	// potentiellement intessante. Elle est de type KWDataGridMerger car elle doit ensuite
+	// etre optimisee selon les algorithmes d'optimisation standard.
+	// On exploite a cet effet des grilles bivariees entre attribut internes pour
+	// obtenir des partitions en VarPart pertinentes
+	void SearchInitialSolution(const KWDataGrid* initialDataGrid, KWDataGrid* initialDataGridSolution) const;
+
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	///// Implementation
+protected:
+	//  Analyse bivariee des paires d'attributs internes
+	// Le resultats est disponible dans bivariateClassStats
+	void ComputeInternalAttributesBivariateStats(const KWDataGrid* initialDataGrid) const;
+
+	// Acces aux analyses bivariees
+	const KWClassStats* GetInternalAttributesBivariateStats() const;
+
+	// Nettoyage des analyse bivariees
+	void CleanInternalAttributesBivariateStats() const;
+
+	// Calcul de l'intersection des discretisations a partir d'un tableau de partition de type KWDGSAttributeDiscretization
+	void ComputeIntersectionDiscretizations(const KWAttributeStats* attributeStats,
+						const ObjectArray* oaAttributeDiscretizations,
+						KWDGSAttributeDiscretization* resultDiscretization) const;
+
+	// Calcul de l'intersection des groupes de valeurs a partir d'un tableau de partition de type KWDGSAttributeGrouping
+	void ComputeIntersectionGroupings(const KWAttributeStats* attributeStats,
+					  const ObjectArray* oaAttributeGroupings,
+					  KWDGSAttributeGrouping* resultGrouping) const;
+
+	// Ecriture d'un rapport JSON a partir des stats bivariee calculees
+	void WriteJSONAnalysisReport(KWClassStats* classStats, const ALString& sReportFileName) const;
+
+	// Specifications d'apprentissage
+	KWLearningSpec* learningSpec;
+
+	// Variables de travail pour l'apprentissage des analyses bivariees
+	mutable KWClassStats bivariateClassStats;
+	mutable KWLearningSpec bivariateLearningSpec;
+};
+
+//////////////////////////////////////////////////////////////////////////////////
+// Classe KWValueSignature
+// Classe technique de gestion des valeurs, impliquees dans un ensemble de partition
+// La signature d'une valeur est le vecteur des index de parties dans chacune
+// des partitions. Cela permet de calculer l'intersection des partitions, dont chaque
+// partie regroupe les valeurs de meme signature
+class KWValueSignature : public Object
+{
+public:
+	// Constructeur
+	KWValueSignature();
+	~KWValueSignature();
+
+	// Valeur
+	void SetValue(Symbol sGroupedValue);
+	Symbol GetValue() const;
+
+	// Signature
+	// Memoire: appartient a l'appele
+	IntVector* GetSignature();
+
+	// Comparaison de la partie signature, puis de la valeur
+	int Compare(const KWValueSignature* aSource) const;
+
+	// Comparaison de la partie signature uniquement
+	int CompareSignature(const KWValueSignature* aSource) const;
+
+	// Affichage, ecriture dans un fichier
+	void Write(ostream& ost) const override;
+
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	///// Implementation
+protected:
+	Symbol sValue;
+	IntVector ivSignature;
+};
+
+// Comparaison de deux signatures, en ignorant la valeur
+int KWValueSignatureCompare(const void* elem1, const void* elem2);
+
+// Implementation en inline
+
+inline KWValueSignature::KWValueSignature() {}
+
+inline KWValueSignature::~KWValueSignature() {}
+
+inline void KWValueSignature::SetValue(Symbol sGroupedValue)
+{
+	sValue = sGroupedValue;
+}
+
+inline Symbol KWValueSignature::GetValue() const
+{
+	return sValue;
+}
+
+inline IntVector* KWValueSignature::GetSignature()
+{
+	return &ivSignature;
+}
